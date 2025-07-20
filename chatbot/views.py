f6652bf34cb8f03dc6ce4a68514fafb8063483ec
Bruna Menezes <brunaads.ti@gmail.com>
2025-07-20 20:24:07 -0300

    feat: Add initial views and URLs for the chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from chatbot.models import Chat
 
-# Create your views here.
+
+def chatbot(request):
+    chats = Chat.objects.all()
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = message
+        return JsonResponse({'message': message, 'response': response})
+
+    return render(request, 'chatbot.html', {'chats': chats})
