7f919e91dee339200fbfca9608f198f2d4a2fb56
Bruna Menezes <brunaads.ti@gmail.com>
2025-07-20 22:20:13 -0300

    feat: Implement AI integration with Groq-Claude

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,30 @@
+import os
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
+from langchain_groq import ChatGroq
+from markdown import markdown
 from chatbot.models import Chat
+
+
+os.environ['GROQ_API_KEY'] = settings.GROQ_API_KEY
+
+
+def ask_ai(message):
+    model = ChatGroq(model='llama-3.3-70b-versatile')
+    message = [
+        (
+            'system',
+            'Você é um assistente responsável por tirar dúvidas sobre programação.'
+            'Responda em formato markdown'
+        ),
+        (
+            'human',
+            message,
+        ),
+    ]
+    response = model.invoke(message)
+    return markdown(response.content, output_format='html')
 
 
 def chatbot(request):
@@ -8,7 +32,7 @@ def chatbot(request):
 
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = message
+        response = ask_ai(message)
         return JsonResponse({'message': message, 'response': response})
 
     return render(request, 'chatbot.html', {'chats': chats})
--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,3 +111,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GROQ_API_KEY = config('GROQ_API_KEY')
